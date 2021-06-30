c86f703640ffd946f1f41e2583680c04cf95093b
Huzi.Wang <huzi.wh@gmail.com>
2021-07-01 00:35:28 +0800

    update: 完善 客户 在职和离职继承 相关 api

--- src/main/java/cn/tobdev/qy/weixin/sdk/api/customer/CustomerTransferApi.java
+++ src/main/java/cn/tobdev/qy/weixin/sdk/api/customer/CustomerTransferApi.java
@@ -1,5 +1,17 @@
 package cn.tobdev.qy.weixin.sdk.api.customer;
 
+import cn.tobdev.qy.weixin.sdk.api.BaseApi;
+import cn.tobdev.qy.weixin.sdk.api.customer.param.GroupChatTransferParam;
+import cn.tobdev.qy.weixin.sdk.api.customer.param.CustomerTransferParam;
+import cn.tobdev.qy.weixin.sdk.api.customer.param.CustomerTransferResultGetParam;
+import cn.tobdev.qy.weixin.sdk.api.customer.param.CustomerUnassignedListParam;
+import cn.tobdev.qy.weixin.sdk.api.customer.resp.CustomerTransferResp;
+import cn.tobdev.qy.weixin.sdk.api.customer.resp.CustomerTransferResultResp;
+import cn.tobdev.qy.weixin.sdk.api.customer.resp.CustomerUnassignedListResp;
+import cn.tobdev.qy.weixin.sdk.api.customer.resp.GroupChatTransferResp;
+import cn.tobdev.qy.weixin.sdk.constant.CustomerApiUris;
+import feign.RequestLine;
+
 /**
  * 客户 在职继承  和 离职继承
  *
@@ -11,6 +23,85 @@ package cn.tobdev.qy.weixin.sdk.api.customer;
  * @version : 1.0
  * @date  : 2021/6/26
  */
-public class CustomerTransferApi {
+public interface CustomerTransferApi extends BaseApi {
+  /**
+   * 分配在职成员的客户
+   * 企业可通过此接口，转接在职成员的客户给其他成员。
+   *
+   * 请求方式：POST（HTTPS）
+   *
+   * 请求地址：https://qyapi.weixin.qq.com/cgi-bin/externalcontact/transfer_customer?access_token=ACCESS_TOKEN
+   *
+   * @param param
+   * @return
+   */
+  @RequestLine(CustomerApiUris.API_CUSTOMER_TRANSFER)
+  CustomerTransferResp transfer(CustomerTransferParam param);
 
+  /**
+   * 查询客户接替状态
+   * 企业和第三方可通过此接口查询在职成员的客户转接情况。
+   *
+   * 请求方式：POST（HTTPS）
+   * 请求地址：https://qyapi.weixin.qq.com/cgi-bin/externalcontact/transfer_result?access_token=ACCESS_TOKEN
+   *
+   * @param param
+   * @return
+   */
+  @RequestLine(CustomerApiUris.API_CUSTOMER_TRANSFER_RESULT)
+  CustomerTransferResultResp getTransferResult(CustomerTransferResultGetParam param);
+
+  /**
+   * 获取待分配的离职成员列表
+   *
+   * 企业和第三方可通过此接口，获取所有离职成员的客户列表，并可进一步调用分配离职成员的客户接口将这些客户重新分配给其他企业成员。
+   *
+   * 请求方式：POST（HTTPS）
+   * 请求地址：https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_unassigned_list?access_token=ACCESS_TOKEN
+   *
+   * @param param
+   * @return
+   */
+  @RequestLine(CustomerApiUris.API_CUSTOMER_UNASSIGNED_LIST)
+  CustomerUnassignedListResp getUnassignedList(CustomerUnassignedListParam param);
+
+  /**
+   * 分配离职成员的客户
+   * 企业可通过此接口，分配离职成员的客户给其他成员。
+   *
+   * 请求方式：POST（HTTPS）
+   * 请求地址：https://qyapi.weixin.qq.com/cgi-bin/externalcontact/resigned/transfer_customer?access_token=ACCESS_TOKEN
+   *
+   * @param param
+   * @return
+   */
+  @RequestLine(CustomerApiUris.API_CUSTOMER_RESIGNED)
+  CustomerTransferResp resigned(CustomerTransferParam param);
+
+  /**
+   * 查询客户接替状态
+   * 企业和第三方可通过此接口查询离职成员的客户分配情况。
+   *
+   * 请求方式：POST（HTTPS）
+   * 请求地址：https://qyapi.weixin.qq.com/cgi-bin/externalcontact/resigned/transfer_result?access_token=ACCESS_TOKEN
+   *
+   * @param param
+   * @return
+   */
+  @RequestLine(CustomerApiUris.API_CUSTOMER_RESIGNED_RESULT)
+  CustomerTransferResultResp getResignedResult(CustomerTransferResultGetParam param);
+
+  /**
+   * 分配离职成员的客户群
+   *
+   * 企业可通过此接口，将已离职成员为群主的群，分配给另一个客服成员。
+   *
+   * 请求方式：POST（HTTPS）
+   * 请求地址：https://qyapi.weixin.qq.com/cgi-bin/externalcontact/groupchat/transfer?access_token=ACCESS_TOKEN
+   *
+   * @param param
+   * @return
+   */
+  @RequestLine(CustomerApiUris.API_CUSTOMER_GROUP_CHAT_TRANSFER)
+  GroupChatTransferResp transferGroupChat(GroupChatTransferParam param);
 }
---
+++ src/main/java/cn/tobdev/qy/weixin/sdk/api/customer/param/CustomerTransferResultGetParam.java
@@ -0,0 +1,36 @@
+package cn.tobdev.qy.weixin.sdk.api.customer.param;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ *
+ * @author : Hui.Wang [huzi.wh@gmail.com]
+ * @version : 1.0
+ * @date  : 2021/6/30
+ */
+@NoArgsConstructor
+@Data
+public class CustomerTransferResultGetParam {
+
+  /**
+   * handoverUserid
+   */
+  @JsonProperty("handover_userid")
+  private String handoverUserid;
+
+  /**
+   * takeoverUserid
+   */
+  @JsonProperty("takeover_userid")
+  private String takeoverUserid;
+
+  /**
+   * cursor
+   */
+  @JsonProperty("cursor")
+  private String cursor;
+}
